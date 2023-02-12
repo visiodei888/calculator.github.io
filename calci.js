960a9d9fbdb283bf881232a46b19fae379ed9b12
visiodei888 <blessy462002@gmail.com>
2023-02-12 09:41:46 +0530

    Calculator using html, css and javascript

---
+++ calci.js
@@ -0,0 +1,78 @@
+var screen=document.querySelector('#screen');
+var btn=document.querySelectorAll('.btn');
+
+for(item of btn)
+{
+    item.addEventListener('click',(e)=>{
+
+        btntext=e.target.innerText;
+        if(btntext=='x')
+        {
+            btntext='*';
+        }
+        if(btntext=='÷')
+        {
+            btntext='/';
+        }
+        screen.value+=btntext;
+    });
+}
+
+function sin()
+{
+    screen.value=Math.sin(screen.value);
+}
+
+function cos()
+{
+    screen.value=Math.cos(screen.value);
+}
+
+function tan()
+{
+    screen.value=Math.tan(screen.value);
+}
+
+function log()
+{
+    screen.value=Math.log(screen.value);
+}
+
+function pow()
+{
+    screen.value=Math.pow(screen.value,2);
+}
+
+function sq()
+{
+    screen.value=Math.sqrt(screen.value,2);
+}
+
+function fact()
+{
+    var i,num,f;
+    f=1;
+    num=screen.value;
+    for(i=1;i<=num;i++)
+    {
+        f=f*i;
+    }
+    i=i-1;
+    screen.value=f;
+}
+
+function backspc()
+{
+    screen.value=screen.value.substr(0,screen.value.length-1);
+}
+
+function pi()
+{
+    screen.value=3.141592653589793238;
+}
+
+function e()
+{
+    screen.value=2.718281828459045;
+}
+
